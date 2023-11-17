6c4f0c7e9c8ce7643a4dbe8b4026a36146e77271
Vadem <VademadoIU8yandex.ru>
2023-11-17 21:55:07 +0300

    add LaboratoryWork_05

---
+++ LaboratoryWork_05/main.cpp
@@ -0,0 +1,13 @@
+#include "sources.h"
+
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "ru");
+
+    int n = std::stoi(argv[1]);
+    print_answer(n, fibonacci(n));
+    std::cout << std::endl;
+    print_answer(n, fibonacci_recursive(n));
+    return 0;
+}
---
+++ LaboratoryWork_05/sources.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+unsigned fibonacci(int n);
+unsigned fibonacci_recursive(int n);
+void print_answer(unsigned n, unsigned fib_n);
